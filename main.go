57c28cb73cb3647c696589652612f678a792a9a9
hariprathap-hp <hariprathap89@gmail.com>
2024-01-25 23:39:00 +0530

    code updated to add Barber's functionalities

--- main.go
+++ main.go
@@ -1,17 +1,34 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/hariprathap-hp/sleeping_barber_dilemma/models"
 )
 
 func main() {
 	shop := models.NewBarberShop()
-
+	//add clients at regular intervals
 	go func() {
-		shop.Customer()
+		shop.ClientEntry()
 	}()
 
-	/*for i := range shop.BarberChan {
+	//Employ the barbers until the shop is closed
+	for i := 1; i <= shop.BarbersEmployed; i++ {
+		isSleeping := false
+		go shop.Barber(isSleeping, i)
+	}
+
+	//close the shop after certain duration
+	go func() {
+		<-time.After(models.ShopclosingTime)
+		shop.IsShopOpen = false
+		fmt.Println("The shop is closed for the day")
+	}()
+
+	fmt.Println("wait ended")
+	for i := range shop.WaitingRoom {
 		fmt.Println("i -- ", i)
-	}*/
+	}
 }
